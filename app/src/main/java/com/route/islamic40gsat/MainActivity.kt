bf6606fa4fd7098300c130ac3134adb19cdd5c2c
Abdallah El-Zoka <abdallah.elzoka@gmail.com>
2024-07-20 10:21:58 +0300

    Testing Github access Tokens

--- app/src/main/java/com/route/islamic40gsat/MainActivity.kt
+++ app/src/main/java/com/route/islamic40gsat/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    // Testing Access Tokens
     // Git & Github (Version Control Systems) (VCS)
     // background => RED
     //   1.0
